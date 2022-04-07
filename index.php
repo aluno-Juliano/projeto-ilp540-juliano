ed2591c0fe020a9e258f62b7279528d86ce446e4
aluno-Juliano <100364195+aluno-Juliano@users.noreply.github.com>
2022-04-07 11:46:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/tela_inicial.css">
+    <title>BARBER SHOP!</title>
+  </head>
+  <body>
+    <div class="rounded">
+      <img src="img/image1.jpg" class="rounded" alt="..."><br>
+      <p class="h1">BARBER SHOP</p><br>
+      <br>
+      <button type="button" class="btn btn-primary btn-lg"><a href="tela_cadastro.html">Cadastro</a></button>
+      <br>
+      <button type="button" class="btn btn-secondary btn-lg"><a href="tela_login.html">Login</a></button>
+    </div>
+    
+    <script src="js/jquery-3.3.1.slim.min.js" ></script>
+    <script src="js/popper.min.js" ></script>
+    <script src="js/bootstrap.min.js" ></script>
+
+  </body>
+</html>
